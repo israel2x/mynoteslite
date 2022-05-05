39a7f2d528080084cff6dce637acc7ac87017f09
israel2x <ezurita@fiec.espol.edu.ec>
2022-05-05 02:14:03 -0500

    Se agregó el footer y el hook useNetwork para verificar la conexión a internet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useNetwork } from "ahooks";
+import React, { useEffect, useState } from "react";
 
 function Footer() {
+  const date = new Date();
+  const [today, setToday] = useState("");
+  const [isOnline, setIsOnline] = useState(false);
+  const networkState = useNetwork();
+
+  function formatDate(dateObject) {
+    const parts = {
+      date: dateObject.getDate(),
+      month: dateObject.getMonth() + 1,
+      year: dateObject.getFullYear(),
+    };
+    return `${parts.date}/${parts.month}/${parts.year}`;
+  }
+
+  useEffect(() => {
+    setToday(formatDate(date));
+    setIsOnline(networkState.online);
+  }, [date, networkState]);
+
   return (
-    <div>
-      <h2>Footer</h2>
+    <div className="footer">
+      <span>Hoy: {today}</span>
+      {isOnline ? (
+        <span className="online">Online</span>
+      ) : (
+        <span className="offline">OffLine</span>
+      )}
     </div>
-  )
+  );
 }
 
 export default Footer;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const getDate = (note) => {
-  return new Date(note.updated).toLocaleDateString();
-}
+  return new Date(note.created).toLocaleDateString();
+};
 
 const getTitle = (note) => {
-  const title = note.body.split('\n')[0];
+  const title = note.body.split("\n")[0];
 
   if (title.length > 45) {
-    return title.slice(0,45);
+    return title.slice(0, 45);
   }
   return title;
-}
+};
 
 const getContent = (note) => {
   let title = getTitle(note);
-  let content = note.body.replaceAll('\n', ' ');
+  let content = note.body.replaceAll("\n", " ");
   content = content.replaceAll(title, "");
 
-  if(content.length > 45) {
-    return content.slice(0,45);
+  if (content.length > 45) {
+    return content.slice(0, 45);
   } else {
     return content;
   }
-}
+};
 
 const ListItem = ({ note }) => {
   return (
-    <Link to={`/note/${note.id}`}>
-      <div className='notes-list-item'>
-        <h3>{getTitle(note)}</h3>
-        <p><span>{getDate(note)}</span>{getContent(note)}</p>
+    <Link to={`/note/${note._id}`}>
+      <div className="notes-list-item">
+        <h3>{note.text}</h3>
+        <p>
+          <span>{getDate(note)}</span>
+          {note.text}
+        </p>
       </div>
     </Link>
-  )
-}
+  );
+};
 
 export default ListItem;
