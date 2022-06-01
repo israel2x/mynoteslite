79e0536a20cc20717984c4f58599fda2157baff0
israel2x <ezurita@fiec.espol.edu.ec>
2022-06-01 00:01:56 -0500

    NotePage, refactorizacion a los metodos que consumen los servicios API

---
+++ src/services/httpService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+//axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log("Error http service", error);
+    alert("An unexpected error ocurred.");
+  }
+
+  return Promise.reject(error);
+});
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
+
+export default http;
